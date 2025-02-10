6bf593cdba56d0aa2d265d4747a73d6b2ff7ffb1
junacipriano <87897220+junacipriano@users.noreply.github.com>
2025-02-10 22:33:42 +0800

    s

--- Vendora/AdminForm.Designer.cs
+++ Vendora/AdminForm.Designer.cs
@@ -28,19 +28,273 @@
         /// </summary>
         private void InitializeComponent()
         {
+            materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            dashboardTab = new TabPage();
+            productTab = new TabPage();
+            panel1 = new Panel();
+            materialButton5 = new MaterialSkin.Controls.MaterialButton();
+            materialButton4 = new MaterialSkin.Controls.MaterialButton();
+            materialButton3 = new MaterialSkin.Controls.MaterialButton();
+            materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            vendorTab = new TabPage();
+            stockEntryTab = new TabPage();
+            stockAdjustmentTab = new TabPage();
+            brandsCategoryTab = new TabPage();
+            recordsTab = new TabPage();
+            salesHistoryTab = new TabPage();
+            userAccountsTab = new TabPage();
+            materialTabControl1.SuspendLayout();
+            productTab.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            materialTabControl1.Controls.Add(dashboardTab);
+            materialTabControl1.Controls.Add(productTab);
+            materialTabControl1.Controls.Add(vendorTab);
+            materialTabControl1.Controls.Add(stockEntryTab);
+            materialTabControl1.Controls.Add(stockAdjustmentTab);
+            materialTabControl1.Controls.Add(brandsCategoryTab);
+            materialTabControl1.Controls.Add(recordsTab);
+            materialTabControl1.Controls.Add(salesHistoryTab);
+            materialTabControl1.Controls.Add(userAccountsTab);
+            materialTabControl1.Depth = 0;
+            materialTabControl1.Dock = DockStyle.Fill;
+            materialTabControl1.Location = new Point(3, 64);
+            materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialTabControl1.Multiline = true;
+            materialTabControl1.Name = "materialTabControl1";
+            materialTabControl1.SelectedIndex = 0;
+            materialTabControl1.Size = new Size(1914, 1013);
+            materialTabControl1.TabIndex = 0;
+            // 
+            // dashboardTab
+            // 
+            dashboardTab.Location = new Point(4, 29);
+            dashboardTab.Name = "dashboardTab";
+            dashboardTab.Padding = new Padding(3);
+            dashboardTab.Size = new Size(1906, 980);
+            dashboardTab.TabIndex = 0;
+            dashboardTab.Text = "Dashboard";
+            dashboardTab.UseVisualStyleBackColor = true;
+            // 
+            // productTab
+            // 
+            productTab.Controls.Add(panel1);
+            productTab.Location = new Point(4, 29);
+            productTab.Name = "productTab";
+            productTab.Padding = new Padding(3);
+            productTab.Size = new Size(1906, 980);
+            productTab.TabIndex = 1;
+            productTab.Text = "Product";
+            productTab.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(materialButton5);
+            panel1.Controls.Add(materialButton4);
+            panel1.Controls.Add(materialButton3);
+            panel1.Controls.Add(materialButton2);
+            panel1.Controls.Add(materialButton1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1900, 974);
+            panel1.TabIndex = 0;
+            // 
+            // materialButton5
+            // 
+            materialButton5.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton5.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton5.Depth = 0;
+            materialButton5.HighEmphasis = true;
+            materialButton5.Icon = null;
+            materialButton5.Location = new Point(824, 40);
+            materialButton5.Margin = new Padding(4, 6, 4, 6);
+            materialButton5.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton5.Name = "materialButton5";
+            materialButton5.NoAccentTextColor = Color.Empty;
+            materialButton5.Size = new Size(158, 36);
+            materialButton5.TabIndex = 4;
+            materialButton5.Text = "materialButton5";
+            materialButton5.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton5.UseAccentColor = false;
+            materialButton5.UseVisualStyleBackColor = true;
+            // 
+            // materialButton4
+            // 
+            materialButton4.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton4.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton4.Depth = 0;
+            materialButton4.HighEmphasis = true;
+            materialButton4.Icon = null;
+            materialButton4.Location = new Point(612, 40);
+            materialButton4.Margin = new Padding(4, 6, 4, 6);
+            materialButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton4.Name = "materialButton4";
+            materialButton4.NoAccentTextColor = Color.Empty;
+            materialButton4.Size = new Size(158, 36);
+            materialButton4.TabIndex = 3;
+            materialButton4.Text = "materialButton4";
+            materialButton4.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton4.UseAccentColor = false;
+            materialButton4.UseVisualStyleBackColor = true;
+            // 
+            // materialButton3
+            // 
+            materialButton3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton3.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton3.Depth = 0;
+            materialButton3.HighEmphasis = true;
+            materialButton3.Icon = null;
+            materialButton3.Location = new Point(422, 40);
+            materialButton3.Margin = new Padding(4, 6, 4, 6);
+            materialButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton3.Name = "materialButton3";
+            materialButton3.NoAccentTextColor = Color.Empty;
+            materialButton3.Size = new Size(158, 36);
+            materialButton3.TabIndex = 2;
+            materialButton3.Text = "materialButton3";
+            materialButton3.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton3.UseAccentColor = false;
+            materialButton3.UseVisualStyleBackColor = true;
+            // 
+            // materialButton2
+            // 
+            materialButton2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton2.Depth = 0;
+            materialButton2.HighEmphasis = true;
+            materialButton2.Icon = null;
+            materialButton2.Location = new Point(206, 106);
+            materialButton2.Margin = new Padding(4, 6, 4, 6);
+            materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton2.Name = "materialButton2";
+            materialButton2.NoAccentTextColor = Color.Empty;
+            materialButton2.Size = new Size(158, 36);
+            materialButton2.TabIndex = 1;
+            materialButton2.Text = "materialButton2";
+            materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton2.UseAccentColor = false;
+            materialButton2.UseVisualStyleBackColor = true;
+            // 
+            // materialButton1
+            // 
+            materialButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton1.Depth = 0;
+            materialButton1.HighEmphasis = true;
+            materialButton1.Icon = null;
+            materialButton1.Location = new Point(66, 106);
+            materialButton1.Margin = new Padding(4, 6, 4, 6);
+            materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton1.Name = "materialButton1";
+            materialButton1.NoAccentTextColor = Color.Empty;
+            materialButton1.Size = new Size(64, 36);
+            materialButton1.TabIndex = 0;
+            materialButton1.Text = "All";
+            materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton1.UseAccentColor = false;
+            materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // vendorTab
+            // 
+            vendorTab.Location = new Point(4, 29);
+            vendorTab.Name = "vendorTab";
+            vendorTab.Size = new Size(1906, 980);
+            vendorTab.TabIndex = 2;
+            vendorTab.Text = "Vendor";
+            vendorTab.UseVisualStyleBackColor = true;
+            // 
+            // stockEntryTab
+            // 
+            stockEntryTab.Location = new Point(4, 29);
+            stockEntryTab.Name = "stockEntryTab";
+            stockEntryTab.Size = new Size(1906, 980);
+            stockEntryTab.TabIndex = 3;
+            stockEntryTab.Text = "Stock Entry";
+            stockEntryTab.UseVisualStyleBackColor = true;
+            // 
+            // stockAdjustmentTab
+            // 
+            stockAdjustmentTab.Location = new Point(4, 29);
+            stockAdjustmentTab.Name = "stockAdjustmentTab";
+            stockAdjustmentTab.Size = new Size(1906, 980);
+            stockAdjustmentTab.TabIndex = 4;
+            stockAdjustmentTab.Text = "Stock Adjustment";
+            stockAdjustmentTab.UseVisualStyleBackColor = true;
+            // 
+            // brandsCategoryTab
+            // 
+            brandsCategoryTab.Location = new Point(4, 29);
+            brandsCategoryTab.Name = "brandsCategoryTab";
+            brandsCategoryTab.Size = new Size(1906, 980);
+            brandsCategoryTab.TabIndex = 5;
+            brandsCategoryTab.Text = "Brands & Category";
+            brandsCategoryTab.UseVisualStyleBackColor = true;
+            // 
+            // recordsTab
+            // 
+            recordsTab.Location = new Point(4, 29);
+            recordsTab.Name = "recordsTab";
+            recordsTab.Size = new Size(1906, 980);
+            recordsTab.TabIndex = 6;
+            recordsTab.Text = "Records";
+            recordsTab.UseVisualStyleBackColor = true;
+            // 
+            // salesHistoryTab
+            // 
+            salesHistoryTab.Location = new Point(4, 29);
+            salesHistoryTab.Name = "salesHistoryTab";
+            salesHistoryTab.Size = new Size(1906, 980);
+            salesHistoryTab.TabIndex = 7;
+            salesHistoryTab.Text = "Sales History";
+            salesHistoryTab.UseVisualStyleBackColor = true;
+            // 
+            // userAccountsTab
+            // 
+            userAccountsTab.Location = new Point(4, 29);
+            userAccountsTab.Name = "userAccountsTab";
+            userAccountsTab.Size = new Size(1906, 980);
+            userAccountsTab.TabIndex = 8;
+            userAccountsTab.Text = "User Accounts";
+            userAccountsTab.UseVisualStyleBackColor = true;
             // 
             // AdminForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(materialTabControl1);
             Name = "AdminForm";
             Text = "AdminForm";
             Load += AdminForm_Load;
+            materialTabControl1.ResumeLayout(false);
+            productTab.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private TabPage dashboardTab;
+        private TabPage productTab;
+        private TabPage vendorTab;
+        private TabPage stockEntryTab;
+        private TabPage stockAdjustmentTab;
+        private TabPage brandsCategoryTab;
+        private TabPage recordsTab;
+        private TabPage salesHistoryTab;
+        private TabPage userAccountsTab;
+        private Panel panel1;
+        private MaterialSkin.Controls.MaterialButton materialButton5;
+        private MaterialSkin.Controls.MaterialButton materialButton4;
+        private MaterialSkin.Controls.MaterialButton materialButton3;
+        private MaterialSkin.Controls.MaterialButton materialButton2;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
     }
 }
